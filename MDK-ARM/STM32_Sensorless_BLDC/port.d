c73d8585f2d6946bdd79571c54b69a762cfaa08f
Mogg-152 <2413979017@qq.com>
2025-09-20 22:16:51 +0800

    first commit

---
+++ MDK-ARM/STM32_Sensorless_BLDC/port.d
@@ -0,0 +1,12 @@
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/port.c
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h
+stm32_sensorless_bldc\port.o: D:\Keil MDK\ARM\ARMCC\Bin\..\include\stddef.h
+stm32_sensorless_bldc\port.o: D:\Keil MDK\ARM\ARMCC\Bin\..\include\stdint.h
+stm32_sensorless_bldc\port.o: ../Core/Inc/FreeRTOSConfig.h
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/RVDS/ARM_CM4F/portmacro.h
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/task.h
+stm32_sensorless_bldc\port.o: ../Middlewares/Third_Party/FreeRTOS/Source/include/list.h
