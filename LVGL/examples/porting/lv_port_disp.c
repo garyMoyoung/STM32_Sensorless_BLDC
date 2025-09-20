713135fa139a784367b3db8a56aa35fc76e05680
Mogg-152 <2413979017@qq.com>
2025-09-20 23:11:09 +0800

    add change to lvgl

--- LVGL/examples/porting/lv_port_disp.c
+++ LVGL/examples/porting/lv_port_disp.c
@@ -4,12 +4,12 @@
  */
 
 /*Copy this file as "lv_port_disp.c" and set this value to "1" to enable content*/
-#if 0
+#if 1
 
 /*********************
  *      INCLUDES
  *********************/
-#include "lv_port_disp_template.h"
+#include "lv_port_disp.h"
 #include <stdbool.h>
 
 /*********************
@@ -98,7 +98,7 @@ void lv_port_disp_init(void)
     static lv_color_t buf_3_1[MY_DISP_HOR_RES * MY_DISP_VER_RES];            /*A screen sized buffer*/
     static lv_color_t buf_3_2[MY_DISP_HOR_RES * MY_DISP_VER_RES];            /*Another screen sized buffer*/
     lv_disp_draw_buf_init(&draw_buf_dsc_3, buf_3_1, buf_3_2,
-                          MY_DISP_VER_RES * LV_VER_RES_MAX);   /*Initialize the display buffer*/
+                          MY_DISP_VER_RES * MY_DISP_HOR_RES);   /*Initialize the display buffer*/
 
     /*-----------------------------------
      * Register the display in LVGL
