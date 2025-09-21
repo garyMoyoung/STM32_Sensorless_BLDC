4e749f20244c05175ee7f1db2dcaf6c3b0f4ee30
Mogg-152 <2413979017@qq.com>
2025-09-21 16:46:37 +0800

    修复LVGL链接错误和MPU9250编译问题

---
+++ App/lcd_task.c
@@ -0,0 +1,16 @@
+#include "lcd_task.h"
+#include "cmsis_os.h"
+#include "lcd.h"
+#include "pic.h"
+#include "math.h"
+
+/* USER CODE BEGIN Header_LcdTask_Entry */
+/**
+* @brief Function show the informations of five-bar
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_LcdTask_Entry */
+
+
+
--- Core/Src/freertos.c
+++ Core/Src/freertos.c
@@ -96,6 +96,7 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
+  
   /* USER CODE END RTOS_THREADS */
 
   /* USER CODE BEGIN RTOS_EVENTS */
